d36484376157e2d8a8cebb7bd7a58cc57bd434da
Pecraft21 <mahmutpektas27@gmail.com>
2026-04-07 16:22:44 +0300

    Create SugarCaneMixin.java

---
+++ .github/src/main/java/com/mahmut/mixin/SugarCaneMixin.java
@@ -0,0 +1,36 @@
+package com.mahmut.mixin;
+
+import net.minecraft.block.*;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SugarCaneBlock.class)
+public abstract class SugarCaneMixin extends Block implements Fertilizable {
+    public SugarCaneMixin(Settings settings) { super(settings); }
+
+    @Override
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state) {
+        BlockPos root = pos;
+        while (world.getBlockState(root.down()).isOf(Blocks.SUGAR_CANE)) root = root.down();
+        int h = 0;
+        while (world.getBlockState(root.up(h)).isOf(Blocks.SUGAR_CANE)) h++;
+        return h < 3;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) { return true; }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        BlockPos root = pos;
+        while (world.getBlockState(root.down()).isOf(Blocks.SUGAR_CANE)) root = root.down();
+        for (int i = 0; i < 3; i++) {
+            BlockPos p = root.up(i);
+            if (world.getBlockState(p).isAir()) world.setBlockState(p, Blocks.SUGAR_CANE.getDefaultState());
+        }
+    }
+}
